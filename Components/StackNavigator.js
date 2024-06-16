78b5f05a4f91c44f9b90382310b8c74d72747b15
Kosti <kosti.launonen70@gmail.com>
2024-06-16 06:37:22 +0300

    Login screen integrated

--- Components/StackNavigator.js
+++ Components/StackNavigator.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../Screens/HomesScreen';
 import ShopScreen from '../Screens/ShopScreen';
+import Login from "../Screens/LoginScreen";
 
 const Stack = createStackNavigator();
 
@@ -9,9 +10,10 @@ export default function StackNavigator() {
 
     //When needed, we can add more screens to the stack navigator
     return (
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator initialRouteName="Login">
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Shop" component={ShopScreen} />
+            <Stack.Screen name="Login" component={Login} />
 
         </Stack.Navigator>
     );
---
+++ Screens/LoginScreen.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Button from '../Components/Button';
+import { TextInput } from 'react-native';
+
+export default function Login({ navigation }) {
+  const [username, setUsername] = React.useState('Username');
+  const [password, setPassword] = React.useState('****')
+
+  // Placeholder account information for logging in
+  const correctUsername = 'User';
+  const correctPassword = '1234';
+
+  const handleLoginPress = () => {
+    if (username === correctUsername && password === correctPassword )
+      {
+        alert('Login succesful!');
+        navigation.navigate('Home');
+      }
+      else if (username == '' || password == '')
+        {
+          alert('Please enter your username and password.')
+        }
+      else{
+        alert(`Wrong username or password.`);
+      }
+
+  };
+
+  const handleRegisterPress = () => {
+    alert("You can't actually register yet, sorry.")
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}> 
+        Please start by logging in, or registering an account.
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setUsername}
+        value={username}
+      />
+      <TextInput
+      style={styles.input}
+      onChangeText={setPassword}
+      value={password}
+      secureTextEntry
+    />
+      <Button label="Login" onPress={handleLoginPress} />
+      <Button label="Register" onPress={handleRegisterPress} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: '#ADD8E6',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    marginBottom: 15,
+      fontSize: 16,
+    },
+    input: {
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    }
+});
